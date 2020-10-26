6766ee10ffbe1c8aec394ac540f9ba30e9d2744a
coffeesp00ns <70654307+coffeesp00ns@users.noreply.github.com>
2020-10-26 12:10:26 -0400

    Add files via upload
    a script.js file that guesses numbers - built via the codecademy fullstack program.

---
+++ script.js
@@ -0,0 +1,33 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => {
+  return Math.floor(Math.random(10)*10);
+}
+
+function compareGuesses(humanGuess,compGuess,target){
+  const humanToTarget = Math.abs(target-humanGuess);
+  const compToTarget = Math.abs(target-compGuess);
+  if (humanToTarget < compToTarget){
+    return true;
+  } else if (compToTarget < humanToTarget){
+    return false;
+  }else {
+    return true;
+  }
+}
+
+const updateScore = (str) => {
+  if (str = 'human') {
+    humanScore += 1;
+  } else if (str = 'computer'){
+    computerScore += 1;
+  }
+}
+
+const advanceRound = ()=> {
+  currentRoundNumber += 1
+}
